9980f207fa0f2affce77a75beac54b4118c5de98
vansy1511 <vansy1511@gmail.com>
2021-12-21 23:58:27 +0700

    First commit

---
+++ FaceMaskDetectionIUH/app/src/main/java/com/iuh/facemaskdetectioniuh/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.iuh.facemaskdetectioniuh
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.iuh.facemaskdetectioniuh.databinding.ActivityMainBinding
+import com.esafirm.imagepicker.features.ImagePicker
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.buttonPhoto.setOnClickListener {
+            ImagePicker.create(this)
+                .single()
+                .showCamera(true)
+                .imageDirectory("Camera")
+                .start()
+        }
+
+        viewModel.drawableLiveData.observe(this) { drawable ->
+            binding.imagePhoto.setImageDrawable(drawable)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+            ImagePicker.getFirstImageOrNull(data)
+                ?.let { image -> viewModel.onActivityResult(this, image) }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
